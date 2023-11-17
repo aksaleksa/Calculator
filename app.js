b297deeb5084740477a6af2214ae9c0a5eec193b
aleksaks27 <aleksaks27@gmail.com>
2023-11-17 18:31:07 +0000

    Make code more concise, add more functions, keyboard interactivity and final styling

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-// To start with, our calculator must have the four main operations:
-// addition, subtraction, multiplication and division. Also, it is
-// essential to have a clear button. These can be defined with functions.
-// In simple terms, a single sum/operation comprises two numbers and an operator
-// The value of the output depends on all of these, so we should save the user's
-// choices in some variables.
-// It is sensible to house the functions within a single object
-// In addition, it would make sense to house the button (operation and number)
-// variables in their own objects
-// An operator (+, -, *, /) can only be accepted once the first number has been chosen
-// Likewise, a second number can only be accepted once the operator is chosen
-// There must be a general calculate/initialize function to deal with this
-// Depending on what a user types, the display of the calculator should update accordingly
-
 const one = document.querySelector("#one");
 const two = document.querySelector("#two");
 const three = document.querySelector("#three");
@@ -23,187 +9,149 @@ const eight = document.querySelector("#eight");
 const nine = document.querySelector("#nine");
 const zero = document.querySelector("#zero");
 
+const numbers = [one, two, three, four, five, six, seven, eight, nine, zero];
+
 const plus = document.querySelector("#plus");
 const minus = document.querySelector("#minus");
 const times = document.querySelector("#times");
 const divide = document.querySelector("#divide");
+const power = document.querySelector("#power");
+const root = document.querySelector("#root");
+const exp = document.querySelector("#exp");
+const log = document.querySelector("#log");
+
+const operators = [plus, minus, times, divide, power, root, exp, log];
+
 const point = document.querySelector("#point");
 const equals = document.querySelector("#equals");
-
 const backspace = document.querySelector("#backspace");
 const clear = document.querySelector("#clear");
 const screen = document.querySelector("#screen");
 
-const operations = {
-    addition: function(num1, num2) {
-        return parseInt(num1) + parseInt(num2);
-    },
-    subtraction: function(num1, num2) {
-        return num1 - num2;
-    },
-    multiplication: function(num1, num2) {
-        return num1 * num2;
-    },
-    division: function(num1, num2) {
-        return num1 / num2;
-    }
-}
-
 function operate(param1, param2, operator) {
-    let output = 0;
+    let output = param1;
     switch(operator) {
         case "+":
-            output = operations.addition(param1, param2);
+            output = Number(param1) + Number(param2);
             break;
         case "-":
-            output = operations.subtraction(param1, param2);
+            output = param1 - param2;
             break;
         case "*":
-            output = operations.multiplication(param1, param2);
+            output = param1 * param2;
             break;
         case "/":
-            output = operations.division(param1, param2);
+            output = param1 / param2;
+            break;
+        case "^":
+            output = Math.pow(param1, param2);
+            break;
+        case "sqrt":
+            output = Math.sqrt(param1);
+            break;
+        case "exp":
+            output = Math.exp(param1);
+            break; 
+        case "ln":
+            output = Math.log(param1);
             break;
     }
     return output
 }
 
-const operators = ["+", "-", "*", "/"];
-
-
-// function updateNumber(number, condition) {
-//     if (operators.includes(screen.textContent)) screen.textContent = "";
-//     screen.textContent += number.textContent;
-//     // screen.textContent = display;
-//     condition = false;
-// }
-
-// function updateOperator(parameter, condition, operator, sign) {
-//     parameter = screen.textContent;
-//     //console.log(parameter);
-//     screen.textContent = sign;
-//     operator = sign;
-//     condition = true;
-// }
+const symbols = ["+", "-", "*", "/", "^", "sqrt", "exp", "ln"];
 
 function display() {
-    //let displayValue = "";
     let param1 = "";
     let param2 = "";
-    let result = "";
-    let operator = "";
-    //let second = false;
+    let result = 0;
+    let sign = "";
 
-    one.addEventListener("click", () => {
-        if (operators.includes(screen.textContent)) screen.textContent = "";
-        screen.textContent += one.textContent;
-    })
-    two.addEventListener("click", () => {
-        if (operators.includes(screen.textContent)) screen.textContent = "";
-        screen.textContent += two.textContent;
-    })
-    three.addEventListener("click", () => {
-        if (operators.includes(screen.textContent)) screen.textContent = "";
-        screen.textContent += three.textContent;
-    })
-    four.addEventListener("click", () => {
-        if (operators.includes(screen.textContent)) screen.textContent = "";
-        screen.textContent += four.textContent;
-    })
-    five.addEventListener("click", () => {
-        if (operators.includes(screen.textContent)) screen.textContent = "";
-        screen.textContent += five.textContent;
-    })
-    six.addEventListener("click", () => {
-        if (operators.includes(screen.textContent)) screen.textContent = "";
-        screen.textContent += six.textContent;
-    })
-    seven.addEventListener("click", () => {
-        if (operators.includes(screen.textContent)) screen.textContent = "";
-        screen.textContent += seven.textContent;
-    })
-    eight.addEventListener("click", () => {
-        if (operators.includes(screen.textContent)) screen.textContent = "";
-        screen.textContent += eight.textContent;
-    })
-    nine.addEventListener("click", () => {
-        if (operators.includes(screen.textContent)) screen.textContent = "";
-        screen.textContent += nine.textContent;
-    })
-    zero.addEventListener("click", () => {
-        if (operators.includes(screen.textContent)) screen.textContent = "";
-        screen.textContent += zero.textContent;
+    for (let number of numbers) {
+        number.addEventListener("click", () => {
+            if (symbols.includes(screen.textContent)) screen.textContent = "";
+            if (screen.textContent.length < 13) screen.textContent += number.textContent;
+        })
+    }
+
+    for (let operator of operators) {
+        operator.addEventListener("click", () => {
+            param1 = screen.textContent;
+            screen.textContent = operator.getAttribute("value");
+            sign = operator.getAttribute("value");
+        })
+    }
+
+    point.addEventListener("click", () => {
+        if (symbols.includes(screen.textContent) || !(screen.textContent.includes("."))) {
+            screen.textContent += ".";
+        } 
     })
 
-    //one.addEventListener("click", () => updateNumber(one, second));
-    //two.addEventListener("click", () => updateNumber(two, second));
-    // three.addEventListener("click", () => updateNumber(three, displayValue, second))
-    // four.addEventListener("click", () => updateNumber(four, displayValue, second))
-    // five.addEventListener("click", () => updateNumber(five, displayValue, second))
-    // six.addEventListener("click", () => updateNumber(six, displayValue, second))
-    // seven.addEventListener("click", () => updateNumber(seven, displayValue, second))
-    // eight.addEventListener("click", () => updateNumber(eight, displayValue, second))
-    // nine.addEventListener("click", () => updateNumber(nine, displayValue, second))
-    // zero.addEventListener("click", () => updateNumber(zero, displayValue, second))
-
-    plus.addEventListener("click", () => {
-        param1 = screen.textContent;
-        screen.textContent = "+";
-        operator = "+";
-    })
-    minus.addEventListener("click", () => {
-        param1 = screen.textContent;
-        screen.textContent = "-";
-        operator = "-";
-    })
-    times.addEventListener("click", () => {
-        param1 = screen.textContent;
-        screen.textContent = "*";
-        operator = "*";
-    })
-    divide.addEventListener("click", () => {
-        param1 = screen.textContent;
-        screen.textContent = "/";
-        operator = "/";
-    })
-    
     equals.addEventListener("click", () => {
-        param2 = screen.textContent;
-        result = operate(param1, param2, operator);
-        screen.textContent = result;
+        if (sign === "sqrt" || sign === "exp" || sign === "log") param2 = 0;
+        else param2 = screen.textContent;
+        result = operate(param1, param2, sign);
+        if (result.toString().length > 12) {
+            screen.textContent = result.toFixed(10);
+        }
+        else screen.textContent = result;
     })
+
+    backspace.addEventListener("click", () => {
+        let tempStr = screen.textContent;
+        tempStr = tempStr.split("").slice(0, tempStr.length - 1).join("");
+        screen.textContent = tempStr;
+    })
+
     clear.addEventListener("click", () => {
         screen.textContent = "";
         param1 = "";
         param2 = "";
-        operator = "";
+        sign = "";
+    })
+
+    // Add keyboard functionality for the calculator
+    document.addEventListener("keypress", (e) => {
+        for (let number of numbers) {
+            if (symbols.includes(screen.textContent)) screen.textContent = "";
+            if (e.key === number.textContent) {
+                screen.textContent += number.textContent;
+            }
+        }
+        for (let operator of operators) {
+            if (e.key === operator.getAttribute("value")) {
+                param1 = screen.textContent;
+                screen.textContent = operator.getAttribute("value");
+                sign = operator.getAttribute("value");
+            }
+        }
+        if (e.key === "=" || e.key === "Enter") {
+            if (sign === "sqrt" || sign === "exp" || sign === "log") param2 = 0;
+            else param2 = screen.textContent;
+            result = operate(param1, param2, sign);
+            if (result.toString().length > 12) {
+                screen.textContent = result.toFixed(10);
+            }
+            else screen.textContent = result;
+        }
+        if (e.key === "b") {
+            let tempStr = screen.textContent;
+            tempStr = tempStr.split("").slice(0, tempStr.length - 1).join("");
+            screen.textContent = tempStr;
+        }
+        if (e.key === "c") {
+            screen.textContent = "";
+            param1 = "";
+            param2 = "";
+            sign = "";
+        }
+        if (e.key === ".") {
+            if (symbols.includes(screen.textContent) || !(screen.textContent.includes("."))) {
+                screen.textContent += ".";
+            } 
+        }
     })
 }
 
 display();
-
-// one.addEventListener("click", () => {
-//     if (second) displayValue = "";
-//     displayValue += one.textContent;
-//     screen.textContent = displayValue;
-//     second = false;
-// })
-// two.addEventListener("click", () => {
-//     if (second) displayValue = "";
-//     displayValue += two.textContent;
-//     screen.textContent = displayValue;
-//     second = false;
-// })
-
-//plus.addEventListener("click", () => {updateOperator(param1, second, operator, "+")})
-//minus.addEventListener("click", () => updateOperator(param1, second, operator, minus));
-//times.addEventListener("click", () => updateOperator(param1, second, operator, times));
-//divide.addEventListener("click", () => updateOperator(param1, second, operator, divide));
-
-// plus.addEventListener("click", () => {
-//     param1 = screen.textContent;
-//     //displayValue = plus.textContent;
-//     screen.textContent = plus.textContent;
-//     second = true;
-//     operator = "+";
-// })
