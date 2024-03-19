2d409cf5ad73b38168b4b4118e13acf75c549231
Aleksandar <aleksaks27@gmail.com>
2024-03-19 00:00:51 +0000

    Make the calculator function well in more use cases. Make the design responsive too

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const seven = document.querySelector("#seven");
 const eight = document.querySelector("#eight");
 const nine = document.querySelector("#nine");
 const zero = document.querySelector("#zero");
+let negative = false;
 
 const numbers = [one, two, three, four, five, six, seven, eight, nine, zero];
 
@@ -26,7 +27,8 @@ const point = document.querySelector("#point");
 const equals = document.querySelector("#equals");
 const backspace = document.querySelector("#backspace");
 const clear = document.querySelector("#clear");
-const screen = document.querySelector("#screen");
+const topDisplay = document.querySelector("#top-display")
+const mainDisplay = document.querySelector("#main-display")
 
 function operate(param1, param2, operator) {
     let output = param1;
@@ -64,91 +66,191 @@ const symbols = ["+", "-", "*", "/", "^", "sqrt", "exp", "ln"];
 function display() {
     let param1 = "";
     let param2 = "";
-    let result = 0;
+    let result;
     let sign = "";
+
+    // Both displays allow a limited number of digits. If an operation symbol is present the main display is wiped 
+    // first and then populated with numbers.
 
     for (let number of numbers) {
         number.addEventListener("click", () => {
-            if (symbols.includes(screen.textContent)) screen.textContent = "";
-            if (screen.textContent.length < 13) screen.textContent += number.textContent;
+            if (symbols.includes(mainDisplay.textContent)) {
+                mainDisplay.textContent = "";
+            }
+            if (mainDisplay.textContent.length < 13) {
+                if (topDisplay.textContent.length > 22) {
+                    mainDisplay.classList.add("main-display-small");
+                    topDisplay.classList.add("top-display-small");
+                }
+                mainDisplay.textContent += number.textContent;
+                topDisplay.textContent += number.textContent;
+            }
         })
     }
 
     for (let operator of operators) {
         operator.addEventListener("click", () => {
-            param1 = screen.textContent;
-            screen.textContent = operator.getAttribute("value");
-            sign = operator.getAttribute("value");
+            // If the sum involves more than two numbers, this block will continually recalculate the parameters and
+            // update the displays.
+            if (param1) {
+                param2 = mainDisplay.textContent;
+                let param3 = operate(param1, param2, sign);
+                param1 = param3;
+                param2 = ""
+                mainDisplay.textContent = operator.getAttribute("value");
+                topDisplay.textContent = `${param1} ${operator.getAttribute("value")} `;
+                sign = mainDisplay.textContent;
+            }
+            else {
+                param1 = mainDisplay.textContent;
+                // This block allows users to start sums with negative numbers.
+                if (topDisplay.textContent[1] === "-") negative = true;
+                if (negative) {
+                    param1 = "-" + param1;
+                    negative = false;
+                }
+                mainDisplay.textContent = operator.getAttribute("value");
+                topDisplay.textContent += ` ${operator.getAttribute("value")} `;
+                sign = mainDisplay.textContent;
+            }
         })
     }
 
+    // Only one decimal point can be added per number.
+
     point.addEventListener("click", () => {
-        if (symbols.includes(screen.textContent) || !(screen.textContent.includes("."))) {
-            screen.textContent += ".";
+        if (!symbols.includes(mainDisplay.textContent)) {
+            if (!mainDisplay.textContent.includes(".")) {
+                mainDisplay.textContent += ".";
+                topDisplay.textContent += ".";
+            }
         } 
     })
 
+    // If the displays get too crowded they switch to a smaller font size. After the calculation the result is printed
+    // and the parameters reset.
+
     equals.addEventListener("click", () => {
         if (sign === "sqrt" || sign === "exp" || sign === "log") param2 = 0;
-        else param2 = screen.textContent;
+        else param2 = mainDisplay.textContent;
         result = operate(param1, param2, sign);
         if (result.toString().length > 12) {
-            screen.textContent = result.toFixed(10);
+            mainDisplay.classList.add("main-display-small");
+            topDisplay.classList.add("top-display-small");
         }
-        else screen.textContent = result;
+        mainDisplay.textContent = result;
+        param1 = "";
+        param2 = "";
     })
 
+    // Backspace works using string/array manipulation. If more space appears, the displays can regain their
+    // larger font sizes.
+
     backspace.addEventListener("click", () => {
-        let tempStr = screen.textContent;
-        tempStr = tempStr.split("").slice(0, tempStr.length - 1).join("");
-        screen.textContent = tempStr;
+        if (topDisplay.textContent.length < 22) {
+            mainDisplay.classList.remove("main-display-small");
+            topDisplay.classList.remove("top-display-small");
+        }
+        let tempStr1 = mainDisplay.textContent;
+        tempStr1 = tempStr1.split("").slice(0, tempStr1.length - 1).join("");
+        tempStr1.split("").slice(0, tempStr1.length - 1).join("");
+        mainDisplay.textContent = tempStr1;
+        let tempStr2 = topDisplay.textContent;
+        tempStr2 = tempStr2.split("").slice(0, tempStr2.length - 1).join("");
+        topDisplay.textContent = tempStr2;
     })
 
     clear.addEventListener("click", () => {
-        screen.textContent = "";
+        mainDisplay.classList.remove("main-display-small");
+        topDisplay.classList.remove("top-display-small");
+        mainDisplay.textContent = "";
+        topDisplay.textContent = "";
         param1 = "";
         param2 = "";
         sign = "";
     })
 
-    // Add keyboard functionality for the calculator
+    // The code below adds keyboard functionality to the calculator and is largely identical to above.
     document.addEventListener("keypress", (e) => {
         for (let number of numbers) {
-            if (symbols.includes(screen.textContent)) screen.textContent = "";
+            if (symbols.includes(mainDisplay.textContent)) {
+                mainDisplay.textContent = "";
+            }
             if (e.key === number.textContent) {
-                screen.textContent += number.textContent;
+                if (mainDisplay.textContent.length < 13) {
+                    if (topDisplay.textContent.length > 22) {
+                        mainDisplay.classList.add("main-display-small");
+                        topDisplay.classList.add("top-display-small");
+                    }
+                    mainDisplay.textContent += number.textContent;
+                    topDisplay.textContent += number.textContent;
+                }
             }
         }
         for (let operator of operators) {
             if (e.key === operator.getAttribute("value")) {
-                param1 = screen.textContent;
-                screen.textContent = operator.getAttribute("value");
-                sign = operator.getAttribute("value");
+                if (param1) {
+                    param2 = mainDisplay.textContent;
+                    let param3 = operate(param1, param2, sign);
+                    param1 = param3;
+                    param2 = ""
+                    mainDisplay.textContent = operator.getAttribute("value");
+                    topDisplay.textContent = `${param1} ${operator.getAttribute("value")} `;
+                    sign = mainDisplay.textContent;
+                }
+                else {
+                    param1 = mainDisplay.textContent;
+                    if (topDisplay.textContent[1] === "-") negative = true;
+                    if (negative) {
+                        param1 = "-" + param1;
+                        negative = false;
+                    }
+                    mainDisplay.textContent = operator.getAttribute("value");
+                    topDisplay.textContent += ` ${operator.getAttribute("value")} `;
+                    sign = mainDisplay.textContent;
+                }
             }
         }
         if (e.key === "=" || e.key === "Enter") {
             if (sign === "sqrt" || sign === "exp" || sign === "log") param2 = 0;
-            else param2 = screen.textContent;
+            else param2 = mainDisplay.textContent;
             result = operate(param1, param2, sign);
             if (result.toString().length > 12) {
-                screen.textContent = result.toFixed(10);
+                mainDisplay.classList.add("main-display-small");
+                topDisplay.classList.add("top-display-small");
             }
-            else screen.textContent = result;
+            mainDisplay.textContent = result;
+            param1 = "";
+            param2 = "";
         }
         if (e.key === "b") {
-            let tempStr = screen.textContent;
-            tempStr = tempStr.split("").slice(0, tempStr.length - 1).join("");
-            screen.textContent = tempStr;
+            if (topDisplay.textContent.length < 22) {
+                mainDisplay.classList.remove("main-display-small");
+                topDisplay.classList.remove("top-display-small");
+            }
+            let tempStr1 = mainDisplay.textContent;
+            tempStr1 = tempStr1.split("").slice(0, tempStr1.length - 1).join("");
+            tempStr1.split("").slice(0, tempStr1.length - 1).join("");
+            mainDisplay.textContent = tempStr1;
+            let tempStr2 = topDisplay.textContent;
+            tempStr2 = tempStr2.split("").slice(0, tempStr2.length - 1).join("");
+            topDisplay.textContent = tempStr2;
         }
         if (e.key === "c") {
-            screen.textContent = "";
+            mainDisplay.classList.remove("main-display-small");
+            topDisplay.classList.remove("top-display-small");
+            mainDisplay.textContent = "";
+            topDisplay.textContent = "";
             param1 = "";
             param2 = "";
             sign = "";
         }
         if (e.key === ".") {
-            if (symbols.includes(screen.textContent) || !(screen.textContent.includes("."))) {
-                screen.textContent += ".";
+            if (!symbols.includes(mainDisplay.textContent)) {
+                if (!mainDisplay.textContent.includes(".")) {
+                    mainDisplay.textContent += ".";
+                    topDisplay.textContent += ".";
+                }
             } 
         }
     })
